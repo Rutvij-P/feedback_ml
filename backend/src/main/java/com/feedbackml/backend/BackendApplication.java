668a0846dfa4b33e98c7ed889d8c50ae08eb828f
Rutu Patel <patelrutvij2801@gmail.com>
2023-12-08 12:41:57 -0800

    initial commit

---
+++ backend/src/main/java/com/feedbackml/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.feedbackml.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
